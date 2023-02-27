ef683ca884ed67f37bd07fb8ad2994adb954956f
leny <info@flatland.be>
2023-02-27 21:58:06 +0100

    Add game stats in convert-data script

--- scripts/convert-data.js
+++ scripts/convert-data.js
@@ -8,26 +8,42 @@ const {readFile, writeFile} = require("fs/promises");
 
 (async () => {
     const scores = await readFile(`${__dirname}/../data/scores.json`, "utf8");
-    const results = {};
+    const results = [];
 
     Object.entries(JSON.parse(scores)).forEach(([date, games]) => {
         const [year, month, day] = date.split("-").map(s => +s);
-        if (!results[year]) {
-            results[year] = {};
-        }
-        if (!results[year][month]) {
-            results[year][month] = {};
-        }
-        if (!results[year][month][day]) {
-            results[year][month][day] = [];
-        }
 
-        results[year][month][day] = games.map(game =>
-            bowling(game).map(frame => ({
-                ...frame,
-                outcome: frame.outcome.split(""),
-            })),
-        );
+        results.push( ...games.map(game => {
+            const parsed = {
+                date: {
+                    year,month,day,
+                },
+                frames: bowling(game).map(frame => ({
+                    ...frame,
+                    outcome: frame.outcome.split("").map((s=>["F","X","/","-"].includes(s)?s:+s)),
+                })),
+            };
+
+            parsed.score = parsed.frames[9].cumulative;
+            parsed.perfect = !!parsed.score===300;
+
+            parsed.stats = {
+                strikes: parsed.frames.reduce((acc,frame)=>acc+(
+                    frame.outcome.reduce((ac,ball)=>ac+(ball==="X"?1:0),0)
+                ),0),
+                spares: parsed.frames.reduce((acc,frame)=>acc+(
+                    frame.outcome.reduce((ac,ball)=>ac+(ball==="/"?1:0),0)
+                ),0),
+                misses: parsed.frames.reduce((acc,frame)=>acc+(
+                    frame.outcome.reduce((ac,ball)=>ac+(ball==="-"?1:0),0)
+                ),0),
+                fouls: parsed.frames.reduce((acc,frame)=>acc+(
+                    frame.outcome.reduce((ac,ball)=>ac+(ball==="F"?1:0),0)
+                ),0),
+            };
+
+            return parsed;
+        }));
     });
 
     await writeFile(
