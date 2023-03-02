885ee4daaa7050a918d0c3134a4f91075d229ae8
leny <info@flatland.be>
2023-03-02 19:38:45 +0100

    Handle new reports format in convert script

--- scripts/convert-data.js
+++ scripts/convert-data.js
@@ -13,37 +13,70 @@ const {readFile, writeFile} = require("fs/promises");
     Object.entries(JSON.parse(scores)).forEach(([date, games]) => {
         const [year, month, day] = date.split("-").map(s => +s);
 
-        results.push( ...games.map(game => {
-            const parsed = {
-                date: {
-                    year,month,day,
-                },
-                frames: bowling(game).map(frame => ({
-                    ...frame,
-                    outcome: frame.outcome.split("").map((s=>["F","X","/","-"].includes(s)?s:+s)),
-                })),
-            };
+        results.push(
+            ...games.map(([ball, game]) => {
+                const parsed = {
+                    date: {
+                        year,
+                        month,
+                        day,
+                    },
+                    ball,
+                    frames: bowling(game).map(frame => ({
+                        ...frame,
+                        outcome: frame.outcome
+                            .split("")
+                            .map(s =>
+                                ["F", "X", "/", "-"].includes(s) ? s : +s,
+                            ),
+                    })),
+                };
 
-            parsed.score = parsed.frames[9].cumulative;
-            parsed.perfect = !!parsed.score===300;
+                parsed.score = parsed.frames[9].cumulative;
+                parsed.perfect = !!parsed.score === 300;
 
-            parsed.stats = {
-                strikes: parsed.frames.reduce((acc,frame)=>acc+(
-                    frame.outcome.reduce((ac,ball)=>ac+(ball==="X"?1:0),0)
-                ),0),
-                spares: parsed.frames.reduce((acc,frame)=>acc+(
-                    frame.outcome.reduce((ac,ball)=>ac+(ball==="/"?1:0),0)
-                ),0),
-                misses: parsed.frames.reduce((acc,frame)=>acc+(
-                    frame.outcome.reduce((ac,ball)=>ac+(ball==="-"?1:0),0)
-                ),0),
-                fouls: parsed.frames.reduce((acc,frame)=>acc+(
-                    frame.outcome.reduce((ac,ball)=>ac+(ball==="F"?1:0),0)
-                ),0),
-            };
+                parsed.stats = {
+                    strikes: parsed.frames.reduce(
+                        (acc, frame) =>
+                            acc +
+                            frame.outcome.reduce(
+                                (ac, ball) => ac + (ball === "X" ? 1 : 0),
+                                0,
+                            ),
+                        0,
+                    ),
+                    spares: parsed.frames.reduce(
+                        (acc, frame) =>
+                            acc +
+                            frame.outcome.reduce(
+                                (ac, ball) => ac + (ball === "/" ? 1 : 0),
+                                0,
+                            ),
+                        0,
+                    ),
+                    misses: parsed.frames.reduce(
+                        (acc, frame) =>
+                            acc +
+                            frame.outcome.reduce(
+                                (ac, ball) => ac + (ball === "-" ? 1 : 0),
+                                0,
+                            ),
+                        0,
+                    ),
+                    fouls: parsed.frames.reduce(
+                        (acc, frame) =>
+                            acc +
+                            frame.outcome.reduce(
+                                (ac, ball) => ac + (ball === "F" ? 1 : 0),
+                                0,
+                            ),
+                        0,
+                    ),
+                };
 
-            return parsed;
-        }));
+                return parsed;
+            }),
+        );
     });
 
     await writeFile(
