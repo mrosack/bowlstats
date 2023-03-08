97cca860aaffe9eed4ed5f09a00b6fc49c09fd10
leny <info@flatland.be>
2023-03-09 00:22:09 +0100

    Refactor Games filtering

--- src/components/games.tsx
+++ src/components/games.tsx
@@ -3,39 +3,78 @@
  * /src/components/games.tsx
  */
 
-import type {FC, ReactElement} from "react";
+import {FC, ReactElement} from "react";
 import type {Game} from "types";
 
 import classnames from "classnames";
 import {useData} from "core/hooks/use-data";
 import GameDetails from "components/game";
+import dayjs from "dayjs";
+import {useState} from "react";
+
+const currentYear: number = new Date().getFullYear();
+const currentMonth: number = new Date().getMonth();
 
 const Games: FC = (): ReactElement => {
     const {games, stats, years} = useData();
 
+    const [year, setYear] = useState<number>(currentYear);
+    const [month, setMonth] = useState<number>(currentMonth);
+
     return (
         <>
-            {years.map((year: number) => (
-                <div key={year}>
-                    <h3 className={classnames("is-size-3", "mb-5")}>{year}</h3>
-                    {games
-                        .filter(
-                            (game: Game): boolean => game.date.year === year,
-                        )
-                        .reverse()
-                        .map(game => (
-                            <GameDetails
-                                key={Object.values(game.date).join("-")}
-                                game={game}
-                                isBest={
-                                    stats.total.best === game.score ||
-                                    (stats.year.year === year &&
-                                        stats.year.best === game.score)
-                                }
-                            />
-                        ))}
-                </div>
-            ))}
+            <div className={classnames("tabs", "is-large")}>
+                <ul>
+                    {years.map((yr: number) => (
+                        <li
+                            key={yr}
+                            className={classnames(year === yr && "is-active")}>
+                            <a
+                                href={"javascript:void(0)"}
+                                onClick={() => setYear(yr)}>
+                                {yr}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className={classnames("tabs")}>
+                <ul>
+                    {Array.from(
+                        new Array(
+                            year < currentYear ? 12 : currentMonth + 1,
+                        ).keys(),
+                    ).map(i => (
+                        <li
+                            key={i}
+                            className={classnames(i === month && "is-active")}>
+                            <a
+                                href={"javascript:void(0)"}
+                                onClick={() => setMonth(i)}>
+                                {dayjs().set("month", i).format("MMM")}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            {games
+                .filter(
+                    (game: Game): boolean =>
+                        game.date.year === year &&
+                        game.date.month === month+1,
+                )
+                .reverse()
+                .map(( game: Game, i:number ) => (
+                    <GameDetails
+                        key={`${Object.values(game.date).join("-")}-${i}`}
+                        game={game}
+                        isBest={
+                            stats.total.best === game.score ||
+                            (stats.year.year === year &&
+                                stats.year.best === game.score)
+                        }
+                    />
+                ))}
         </>
     );
 };
