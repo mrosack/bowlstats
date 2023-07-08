2312a873f8bba6cca0998331cfd96c3551976837
leny <info@flatland.be>
2023-07-08 22:28:58 +0200

    Refactor data encoding & parsing

--- scripts/convert-data.js
+++ scripts/convert-data.js
@@ -22,149 +22,162 @@ const firstBallValue = outcome => {
     const results = [];
 
     parseAllDocuments(scores).forEach(doc => {
-        const {date, games} = doc.toJS();
+        const {
+            date,
+            games,
+            ball: baseBall,
+            oilPattern: baseOilPattern,
+        } = doc.toJS();
         const [year, month, day] = date.split("-").map(s => +s);
 
         results.push(
-            ...games.map(({ball, oilPattern, frames, note}) => {
-                const splits = [];
-                const parsed = {
-                    date: {
-                        year,
-                        month,
-                        day,
-                    },
-                    ball: String(ball),
-                    oilPattern: oilPattern || false,
-                    frames: bowling(
-                        frames
-                            .split(",")
-                            .map(frame => frame.trim())
-                            .map((frame, index) => {
-                                if (frame.startsWith("S")) {
-                                    splits.push(index);
-                                    return frame.substring(1);
-                                }
-                                return frame;
-                            }),
-                    ).map((frame, index) => {
-                        const outcome = frame.outcome
-                            .split("")
-                            .map(s =>
-                                ["F", "X", "/", "-"].includes(s) ? s : +s,
-                            );
+            ...games.map(
+                ({
+                    ball = baseBall,
+                    oilPattern = baseOilPattern,
+                    frames,
+                    note,
+                }) => {
+                    const splits = [];
+                    const parsed = {
+                        date: {
+                            year,
+                            month,
+                            day,
+                        },
+                        ball: String(ball),
+                        oilPattern: oilPattern || false,
+                        frames: bowling(
+                            frames
+                                .split(",")
+                                .map(frame => frame.trim())
+                                .map((frame, index) => {
+                                    if (frame.startsWith("S")) {
+                                        splits.push(index);
+                                        return frame.substring(1);
+                                    }
+                                    return frame;
+                                }),
+                        ).map((frame, index) => {
+                            const outcome = frame.outcome
+                                .split("")
+                                .map(s =>
+                                    ["F", "X", "/", "-"].includes(s) ? s : +s,
+                                );
 
-                        let split = false,
-                            pins = outcome.reduce((acc, oc) => {
-                                if (typeof oc === "number") {
-                                    return acc + oc;
-                                }
-                                if (oc === "X") {
-                                    return acc + 10;
-                                }
-                                if (oc === "/") {
-                                    return acc + 10 - acc;
-                                }
-                                return acc;
-                            }, 0),
-                            spare = outcome.reduce((v, oc, i, arr) => {
-                                if (oc === "/" && i > 0) {
-                                    return arr[i - 1] === 9
-                                        ? "single"
-                                        : "multiple";
-                                }
-                                return v;
-                            }, false);
+                            let split = false,
+                                pins = outcome.reduce((acc, oc) => {
+                                    if (typeof oc === "number") {
+                                        return acc + oc;
+                                    }
+                                    if (oc === "X") {
+                                        return acc + 10;
+                                    }
+                                    if (oc === "/") {
+                                        return acc + 10 - acc;
+                                    }
+                                    return acc;
+                                }, 0),
+                                spare = outcome.reduce((v, oc, i, arr) => {
+                                    if (oc === "/" && i > 0) {
+                                        return arr[i - 1] === 9
+                                            ? "single"
+                                            : "multiple";
+                                    }
+                                    return v;
+                                }, false);
 
-                        if (splits.includes(index)) {
-                            split = outcome[1] === "/" ? "converted" : "yes";
-                        }
+                            if (splits.includes(index)) {
+                                split =
+                                    outcome[1] === "/" ? "converted" : "yes";
+                            }
 
-                        return {
-                            ...frame,
-                            outcome,
-                            pins,
-                            split,
-                            spare,
-                            open: pins < 10,
-                        };
-                    }),
-                };
+                            return {
+                                ...frame,
+                                outcome,
+                                pins,
+                                split,
+                                spare,
+                                open: pins < 10,
+                            };
+                        }),
+                    };
 
-                if (note) {
-                    parsed.note = note;
-                }
+                    if (note) {
+                        parsed.note = note;
+                    }
 
-                parsed.score = parsed.frames[9].cumulative;
-                parsed.pins = parsed.frames.reduce(
-                    (acc, frame) => acc + frame.pins,
-                    0,
-                );
-                parsed.perfect = parsed.score === 300;
+                    parsed.score = parsed.frames[9].cumulative;
+                    parsed.pins = parsed.frames.reduce(
+                        (acc, frame) => acc + frame.pins,
+                        0,
+                    );
+                    parsed.perfect = parsed.score === 300;
 
-                parsed.stats = {
-                    avgFirstBallPinfall: +(
-                        parsed.frames.reduce(
-                            (acc, frame) =>
-                                acc + firstBallValue(frame.outcome[0]),
-                            0,
-                        ) / 10
-                    ).toFixed(2),
-                    splits: {
-                        total: splits.length,
-                        converted: parsed.frames.reduce(
-                            (acc, frame) =>
-                                acc + (frame.split === "converted" ? 1 : 0),
-                            0,
-                        ),
-                    },
-                    strikes: parsed.frames.reduce(
-                        (acc, frame) =>
-                            acc +
-                            frame.outcome.reduce(
-                                (ac, ball) => ac + (ball === "X" ? 1 : 0),
+                    parsed.stats = {
+                        avgFirstBallPinfall: +(
+                            parsed.frames.reduce(
+                                (acc, frame) =>
+                                    acc + firstBallValue(frame.outcome[0]),
+                                0,
+                            ) / 10
+                        ).toFixed(2),
+                        splits: {
+                            total: splits.length,
+                            converted: parsed.frames.reduce(
+                                (acc, frame) =>
+                                    acc + (frame.split === "converted" ? 1 : 0),
                                 0,
                             ),
-                        0,
-                    ),
-                    spares: {
-                        total: parsed.frames.reduce(
-                            (acc, frame) => acc + (frame.spare ? 1 : 0),
-                            0,
-                        ),
-                        single: parsed.frames.reduce(
+                        },
+                        strikes: parsed.frames.reduce(
                             (acc, frame) =>
-                                acc + (frame.spare === "single" ? 1 : 0),
+                                acc +
+                                frame.outcome.reduce(
+                                    (ac, ball) => ac + (ball === "X" ? 1 : 0),
+                                    0,
+                                ),
                             0,
                         ),
-                        multiple: parsed.frames.reduce(
-                            (acc, frame) =>
-                                acc + (frame.spare === "multiple" ? 1 : 0),
-                            0,
-                        ),
-                    },
-                    misses: parsed.frames.reduce(
-                        (acc, frame) =>
-                            acc +
-                            frame.outcome.reduce(
-                                (ac, ball) => ac + (ball === "-" ? 1 : 0),
+                        spares: {
+                            total: parsed.frames.reduce(
+                                (acc, frame) => acc + (frame.spare ? 1 : 0),
                                 0,
                             ),
-                        0,
-                    ),
-                    fouls: parsed.frames.reduce(
-                        (acc, frame) =>
-                            acc +
-                            frame.outcome.reduce(
-                                (ac, ball) => ac + (ball === "F" ? 1 : 0),
+                            single: parsed.frames.reduce(
+                                (acc, frame) =>
+                                    acc + (frame.spare === "single" ? 1 : 0),
                                 0,
                             ),
-                        0,
-                    ),
-                };
+                            multiple: parsed.frames.reduce(
+                                (acc, frame) =>
+                                    acc + (frame.spare === "multiple" ? 1 : 0),
+                                0,
+                            ),
+                        },
+                        misses: parsed.frames.reduce(
+                            (acc, frame) =>
+                                acc +
+                                frame.outcome.reduce(
+                                    (ac, ball) => ac + (ball === "-" ? 1 : 0),
+                                    0,
+                                ),
+                            0,
+                        ),
+                        fouls: parsed.frames.reduce(
+                            (acc, frame) =>
+                                acc +
+                                frame.outcome.reduce(
+                                    (ac, ball) => ac + (ball === "F" ? 1 : 0),
+                                    0,
+                                ),
+                            0,
+                        ),
+                    };
 
-                return parsed;
-            }),
+                    return parsed;
+                },
+            ),
         );
     });
 
